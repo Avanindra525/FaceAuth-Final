892b60d080c6d0cc83ae100a48d0f6f59cab3e1b
user01 <user01@RCP-DSA145.bhel.in>
2026-07-28 11:07:36 +0530

    Add home route

--- web/lib/api.ts
+++ web/lib/api.ts
@@ -1,6 +1,6 @@
 export const API_BASE =
   process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/$/, "") ||
-  "http://127.0.0.1:5000";
+  "process.env.NEXT_PUBLIC_API_URL";
 
 type ApiOptions = {
   token?: string;
